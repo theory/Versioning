b35a925bd9ad326acfe8fa52b9ab0dbc7611b098
Hubert depesz Lubaczewski <depesz@depesz.com>
2010-08-22 16:23:19 +0200

    Add a bit more informative comment in it

--- tools/list-dependencies-from-patches.sh
+++ tools/list-dependencies-from-patches.sh
@@ -2,7 +2,8 @@
 
 # Simple tool to list dependencies in form suitable for tsort utility.
 # Run this script like this:
-#   /some/path/list-dependencies-from-patches.sh *.sql
+#   /some/path/list-dependencies-from-patches.sh *.sql | tsort | tac
+# To get patches in order that satisfies dependencies while loading them.
 
 grep -hiE '^[[:space:]]*select _v.register_patch\(' "$@" | \
     sed 's/^[^(]*(//' | while read LINE
